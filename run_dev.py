af63e7231db30c0b955f4c347615b2f837e54b1f
mstne03 <mserraitorne@gmail.com>
2026-05-07 13:50:40 +0200

    feat: last changes

--- run_dev.py
+++ run_dev.py
@@ -16,9 +16,10 @@
 
 import uvicorn
 
-uvicorn.run(
-    "main:app",
-    host="127.0.0.1",
-    port=8001,
-    reload=True,
-)
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8001,
+        reload=True,
+    )
